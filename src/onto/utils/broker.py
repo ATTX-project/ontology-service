a8a236ac1d28c3dfb75f04d8b6dbfd6563455bcf
Stefan Negru <stefan@blankdots.com>
2018-03-21 08:30:00 +0200

    refactor to Jena-based the ontology-service

--- src/onto/utils/broker.py
+++
@@ -1,7 +0,0 @@
-from os import environ
-
-broker = {'host': environ['MHOST'] if 'MHOST' in environ else "localhost",
-          'user': environ['MUSER'] if 'MUSER' in environ else "user",
-          'provqueue': environ['MPROVQUEUE'] if 'MPROVQUEUE' in environ else "provenance.inbox",
-          'rpcqueue': environ['MRPCQUEUE'] if 'MRPCQUEUE' in environ else "attx.ontology.inbox",
-          'pass': environ['MKEY'] if 'MKEY' in environ else "password"}
